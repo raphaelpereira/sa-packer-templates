b478340a0157a5bb9d41b960189f24c32e91f61a
Raphael Pereira <raphaelpereira@gmail.com>
2016-06-24 15:35:26 -0300

    Version 1.3.16
    * Corrected boost build
    * Included host PCSC lite

--- scripts/smartaccess/boost.sh
+++ scripts/smartaccess/boost.sh
@@ -3,15 +3,23 @@
 set -e
 set -x
 
-cd /usr/local/smartaccess
-sudo chroot besav2rx-dev-filesystem/ apt-get install -y libbz2-dev libssl-dev
-sudo git clone https://github.com/boostorg/boost.git --branch boost-1.61.0 --single-branch --depth 1 besav2rx-dev-filesystem/root/boost
-
-sudo chroot besav2rx-dev-filesystem/ "
+echo "
+#!/bin/bash
 cd /root/boost
-./bootstrap
+./bootstrap.sh
 ./b2 install
 cd ..
 rm -rf boost
-"
+" > make_boost
+chmod 777 make_boost
+sudo mv make_boost /usr/local/smartaccess/besav2rx-dev-filesystem/
+wget -O boost.tar.bz2 http://downloads.sourceforge.net/project/boost/boost/1.61.0/boost_1_61_0.tar.bz2
+tar xjf boost.tar.bz2
+mv boost_1_61_0 boost
+sudo mv boost /usr/local/smartaccess/besav2rx-dev-filesystem/root
+cd /usr/local/smartaccess
+sudo chroot besav2rx-dev-filesystem/ apt-get install -y libbz2-dev libssl-dev
+sudo chroot besav2rx-dev-filesystem/ /make_boost
+sudo rm -rf besav2rx-dev-filesystem/make_boost
+
 cd ~
--- scripts/smartaccess/pcsc.sh
+++ scripts/smartaccess/pcsc.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+sudo apt-get install -y libpcsclite-dev pcscd
+
 cd /usr/local/smartaccess
 sudo chroot besav2rx-dev-filesystem/ apt-get install -y libpcsclite-dev
 cd ~
